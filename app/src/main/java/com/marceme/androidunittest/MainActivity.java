9e9690aa115dd4e4514d463c7ea559a17b2414ae
Marcelino Yax <marce7j@gmail.com>
2016-07-05 12:03:14 -0400

    Added button click listener.

--- app/src/main/java/com/marceme/androidunittest/MainActivity.java
+++ app/src/main/java/com/marceme/androidunittest/MainActivity.java
@@ -2,6 +2,10 @@ package com.marceme.androidunittest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final TextView display = (TextView)findViewById(R.id.displayText);
+        final EditText editor = (EditText) findViewById(R.id.messageEditor);
+        Button send = (Button) findViewById(R.id.sendButton);
+
+        send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String message = editor.getText().toString();
+                message = message.trim();
+
+                display.setText(message);
+
+            }
+        });
+
+
     }
 }
